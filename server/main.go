19b457c8dc9c6bc4453dd176f35c68b035a8effb
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-03-05 22:43:07 +0530

    Added server connection code

--- server/main.go
+++ server/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
-	"gorm.io/gorm"
+	"fulfillment/database"
 	pb "fulfillment/fulfillment"
+	"log"
+	"net"
 
+	"google.golang.org/grpc"
+	"gorm.io/gorm"
 )
 
-type DeliveryServer struct {
-	DB *gorm.DB
+type Server struct {
 	pb.FulFillmentServer
+	DB *gorm.DB
 }
 
-type UserServer struct {
-	DB *gorm.DB
-	pb.FulFillmentServer
+
+func main() {
+	db := database.Connection()
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterFulFillmentServer(grpcServer, &Server{DB : db})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+	log.Println("Server started running on port 9000")
 }
